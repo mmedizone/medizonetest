03847f0226e577ec5f0c986a2d1981684a1672ee
mmedizone <mmedizone@gmail.com>
2023-04-21 22:37:21 +0700

    added firebase auth

--- login.js
+++ login.js
@@ -1,3 +1,17 @@
-function login(){
-    window.location.href = 'dashboard.html';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+function loginFirebase(email,password){
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        window.location.href = 'dashboard.html';
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
 }
